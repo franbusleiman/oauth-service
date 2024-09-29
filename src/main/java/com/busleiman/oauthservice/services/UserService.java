30b18320657efbb737f10eedf2df03e95caa2ee3
JuanQuinteros04 <juanmanuelquinteros3@gmail.com>
2024-09-29 11:04:48 -0300

    Changing login to search by email

--- src/main/java/com/busleiman/oauthservice/services/UserService.java
+++ src/main/java/com/busleiman/oauthservice/services/UserService.java
@@ -25,20 +25,20 @@ public class UserService implements IUserService, UserDetailsService {
     UserFeignClient userFeignClient;
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
 
-        ResponseEntity<User> userResponseEntity = userFeignClient.findUserByEmail(username);
+        ResponseEntity<User> userResponseEntity = userFeignClient.findUserByEmail(email);
 
         if(userResponseEntity.getStatusCode().isError()){
 
-            logger.info(String.format("User %s not found"), username);
+            logger.info(String.format("User %s not found"), email);
 
             throw new RuntimeException("User not found");
         }
 
         User user = userResponseEntity.getBody();
 
-        logger.info(String.format("User found"), username);
+        logger.info(String.format("User found"), email);
 
 
         List<GrantedAuthority> grantedAuthorities = user.getRoles().stream()
